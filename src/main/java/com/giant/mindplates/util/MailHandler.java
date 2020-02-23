26289b2544e5bfad04872b4e5b901be23afaf754
hyunseok <mostgreat@gmail.com>
2020-02-23 17:16:03 +0900

    mail 기능 추가.

--- src/main/java/com/giant/mindplates/util/MailHandler.java
+++ src/main/java/com/giant/mindplates/util/MailHandler.java
@@ -1,8 +1,16 @@
 package com.giant.mindplates.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 
@@ -12,19 +20,35 @@ public class MailHandler {
 	
 	@Autowired
     private JavaMailSender javaMailSender;
-	
-	public void sendEmail() {
 
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("to_1@gmail.com", "to_2@gmail.com", "to_3@yahoo.com");
+	@Async
+	public void sendEmail(String receiver, String subject, String contents) throws MessagingException, IOException {
 
-        msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n Spring Boot Email");
+        MimeMessage msg = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(msg, false);
+        helper.setTo(receiver);
+        helper.setSubject(subject);
+        helper.setText(contents);
 
         javaMailSender.send(msg);
-
-    }
+	}
 	
+	@Async
+	public void sendEmail(String receiver, String subject, String contents, String attachedResourcePath, String attachedResoucreName) throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo(receiver);
+        helper.setSubject(subject);
+
+        // true = text/html
+        helper.setText(contents, true);
+
+        FileSystemResource file = new FileSystemResource(new File("path/android.png"));
+        helper.addAttachment(attachedResoucreName, file);
+        javaMailSender.send(msg);
+	}
 	
 
 }
