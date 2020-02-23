a2c24d3164bd874984e7654c701c8c39d5d7d84a
ildong <ildong@DESKTOP-GQ6LND2>
2020-02-23 23:10:57 +0900

    약간의 패키지 수정

--- src/main/java/com/giant/mindplates/biz/user/controller/UserController.java
+++ src/main/java/com/giant/mindplates/biz/user/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.giant.mindplates.biz.user.controller;
 
-import com.giant.mindplates.biz.user.entity.User;
-import com.giant.mindplates.biz.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.validation.Valid;
-import java.util.Date;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.giant.mindplates.biz.user.entity.User;
+import com.giant.mindplates.biz.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,12 +23,11 @@ public class UserController {
 
     @PostMapping("")
     public User create(@Valid @RequestBody User user) {
-        Date now = new Date();
         user.setActivateYn(false);
         user.setDeleteYn(false);
         user.setUseYn(true);
-        user.setCreationDate(now);
-        user.setLastUpdateDate(now);
+        user.setCreationDate(LocalDateTime.now());
+        user.setLastUpdateDate(LocalDateTime.now());
         userService.save(user);
         return user;
     }
