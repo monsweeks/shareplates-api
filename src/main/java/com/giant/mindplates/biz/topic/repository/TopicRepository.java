5f909efba24dfc8b2858af94c6c4dc59fa756bd7
mindplates <mindplates@gmail.com>
2020-03-09 03:38:33 +0900

    토픽 삭제, 토픽 목록, 상세 조회시 useYn=1인 경우만 조회하도록 변경,

--- src/main/java/com/giant/mindplates/biz/topic/repository/TopicRepository.java
+++ src/main/java/com/giant/mindplates/biz/topic/repository/TopicRepository.java
@@ -3,9 +3,14 @@ package com.giant.mindplates.biz.topic.repository;
 import com.giant.mindplates.biz.topic.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
     Long countByOrganizationIdAndName(Long organizationId, String name);
+    List<Topic> findAllByUseYnTrue();
+    Optional<Topic> findByIdAndUseYnTrue(Long id);
 
 }
 
