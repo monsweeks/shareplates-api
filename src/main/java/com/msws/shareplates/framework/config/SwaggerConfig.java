62a7ec87f641a82a03ebdb0cfaa7a027a3138da2
hyunseok <mostgreat@gmail.com>
2020-03-23 21:54:08 +0900

    swagger config base package  변경.

--- src/main/java/com/msws/shareplates/framework/config/SwaggerConfig.java
+++ src/main/java/com/msws/shareplates/framework/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.giant.mindplates.biz"))
+                .apis(RequestHandlerSelectors.basePackage("com.msws.shareplates.biz"))
                 .paths(PathSelectors.any())
                 .build();
     }
