962d3fe2bf408417957ae1ad1efaf17600af3153
mindplates <mindplates@gmail.com>
2020-03-29 05:14:56 +0900

    챕터 순서 변경 api 추가 챕터 컨트롤러 클래스명 변경

--- src/main/java/com/msws/shareplates/biz/chapter/repository/ChapterRepository.java
+++ src/main/java/com/msws/shareplates/biz/chapter/repository/ChapterRepository.java
@@ -1,12 +1,19 @@
 package com.msws.shareplates.biz.chapter.repository;
 
+import com.msws.shareplates.biz.chapter.entity.Chapter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface ChapterRepository extends JpaRepository<Chapter, Long> {
 
-import com.msws.shareplates.biz.chapter.entity.Chapter;
+    List<Chapter> findByTopicIdOrderByOrderNo(long topicId);
 
-public interface ChapterRepository extends  JpaRepository<Chapter, Long>{
 
-	List<Chapter> findByTopicIdOrderByOrderNo(long topicId);
+    @Modifying
+    @Query("UPDATE Chapter c SET c.orderNo = :orderNo WHERE c.topic.id = :topicId AND c.id = :chapterId")
+    void updateChapterOrder(@Param("topicId") Long topicId, @Param("chapterId") Long chapterId, @Param("orderNo") Integer orderNo);
 }
--- src/main/java/com/msws/shareplates/biz/chapter/service/ChapterService.java
+++ src/main/java/com/msws/shareplates/biz/chapter/service/ChapterService.java
@@ -8,80 +8,85 @@ import com.msws.shareplates.common.exception.code.ServiceExceptionCode;
 import com.msws.shareplates.framework.session.vo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class ChapterService {
-	
-	@Autowired
-	private ChapterRepository chapterRepository;
-	
-	@Autowired
-	private TopicRepository topicRepository;
 
-	public Chapter saveChapter(Chapter chapter, UserInfo userInfo) {
-		topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
-						
-			if(topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
-				throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
+    @Autowired
+    private ChapterRepository chapterRepository;
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public Chapter saveChapter(Chapter chapter, UserInfo userInfo) {
+        topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
+
+            if (topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
+                throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
 
 
-			
-			return topic;
-		}).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
+            return topic;
+        }).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
 
-		chapter = chapterRepository.save(chapter);
-		return chapter;
-		
-	}
-	
-	public void deleteChapter(Chapter chapter, UserInfo userInfo) {
-		
-		topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
-			
-			if(topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
-				throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
-			
-			chapterRepository.delete(chapter);
-			
-			return topic;
-		}).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
-	}
-	
-	public List<Chapter> getChapters(Chapter chapter, UserInfo userInfo){
-		
-		return topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
-			
-			if(topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
-				throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);			
-			
-			return chapterRepository.findByTopicIdOrderByOrderNo(chapter.getTopic().getId());
-		}).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
-		
-	}
-	
-	public Chapter getChapter(long chapterId, Chapter chapter, UserInfo userInfo) {
-		
-		return topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
-			
-			if(topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
-				throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);	
-			
-			return chapterRepository.findById(chapterId);
-		}).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC))
-				.orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_CHAPTER));
-	}
+        chapter = chapterRepository.save(chapter);
+        return chapter;
 
-	public Chapter getChapter(long chapterId, UserInfo userInfo) {
+    }
 
-		Chapter chapter = chapterRepository.findById(chapterId).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_CHAPTER));
+    @Transactional
+    public void updateChapterOrders(Long topicId, List<Chapter> chapters) {
+        chapters.stream().forEach(chapter -> chapterRepository.updateChapterOrder(topicId, chapter.getId(), chapter.getOrderNo()));
+    }
 
-		if (chapter.getTopic().getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId()))) {
-			throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
-		}
+    public void deleteChapter(Chapter chapter, UserInfo userInfo) {
 
-		return chapter;
+        topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
 
-	}
+            if (topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
+                throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
+
+            chapterRepository.delete(chapter);
+
+            return topic;
+        }).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
+    }
+
+    public List<Chapter> getChapters(Chapter chapter, UserInfo userInfo) {
+
+        return topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
+
+            if (topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
+                throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
+
+            return chapterRepository.findByTopicIdOrderByOrderNo(chapter.getTopic().getId());
+        }).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC));
+
+    }
+
+    public Chapter getChapter(long chapterId, Chapter chapter, UserInfo userInfo) {
+
+        return topicRepository.findById(chapter.getTopic().getId()).map(topic -> {
+
+            if (topic.getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId())))
+                throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
+
+            return chapterRepository.findById(chapterId);
+        }).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_TOPIC))
+                .orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_CHAPTER));
+    }
+
+    public Chapter getChapter(long chapterId, UserInfo userInfo) {
+
+        Chapter chapter = chapterRepository.findById(chapterId).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_EXISTS_CHAPTER));
+
+        if (chapter.getTopic().getTopicUsers().stream().noneMatch(topicUser -> topicUser.getUser().getId().equals(userInfo.getId()))) {
+            throw new ServiceException(ServiceExceptionCode.RESOURCE_NOT_AUTHORIZED);
+        }
+
+        return chapter;
+
+    }
 }
