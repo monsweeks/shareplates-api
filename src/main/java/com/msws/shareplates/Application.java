2983a4c2765dcc24e3c4bdf186e2c555f4aaa80e
hyunseok <mostgreat@gmail.com>
2020-05-01 15:42:40 +0900

    data insert ( influx db, elasticsearch )

--- src/main/java/com/msws/shareplates/Application.java
+++ src/main/java/com/msws/shareplates/Application.java
@@ -1,5 +1,7 @@
 package com.msws.shareplates;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,17 +11,37 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 import com.msws.shareplates.framework.config.FileConfig;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableConfigurationProperties({
     FileConfig.class
 })
 @EnableAsync
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class Application extends SpringBootServletInitializer {
-
+public class Application extends SpringBootServletInitializer implements CommandLineRunner{
+	
+	@Value("${spring.profiles.active}")
+    private String activeProfile;
+	
+	@Value("${file.upload-dir}")
+    private String fileupload_dir;
+	
     //스프링부트 톰캣 이용시
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("stared as {}", activeProfile);
+		log.info("fileupload dir {}", fileupload_dir);
+
+		//code to do
+		
+	}
+    
+    
 
 }
---
+++ src/main/java/com/msws/shareplates/biz/statistic/service/ElasticsearchService.java
@@ -0,0 +1,55 @@
+package com.msws.shareplates.biz.statistic.service;
+
+import java.util.List;
+
+import org.jboss.logging.MDC;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.msws.shareplates.biz.statistic.enums.Stat_database;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class ElasticsearchService implements StatServiceIF<Object>{
+
+	@Override
+	public Stat_database getName() {
+		return Stat_database.elasticsearch;
+	}
+	
+	@Override
+	public void setData(Object data) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public List<Object> getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public synchronized void setData(Object data, String index_name) {
+		
+		MDC.put("index_name", index_name);
+		if( data instanceof List) {
+			for( Object row : (List<Object>) data) {
+				log.info("{}", new Gson().toJson(row));
+			}
+		}else {
+			log.info("{}", new Gson().toJson(data));
+		}
+		MDC.clear();
+	}
+
+	
+
+	
+	
+	
+	
+
+
+}
