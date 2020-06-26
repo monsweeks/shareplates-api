3b7a0f165a711059714998ee5204b9ca2f60d3b7
hyunseok <mostgreat@gmail.com>
2020-06-26 22:01:52 +0900

    timespan 변수 추가.

--- src/main/java/com/msws/shareplates/biz/statistic/service/StatServiceIF.java
+++ src/main/java/com/msws/shareplates/biz/statistic/service/StatServiceIF.java
@@ -9,7 +9,7 @@ import com.msws.shareplates.biz.statistic.enums.Stat_database;
 public interface StatServiceIF<T> {
 	
 	public Stat_database getName();
-	public List<T> getData(String key, String value, TimeUnit timeunit, int amount);
+	public List<T> getData(String key, String value, TimeUnit timeunit, int amount, String timespan);
 	public List<T> getDetailData(String key, String value, TimeUnit timeunit, int amount);
 	public void setData(Share data, Long userId, String flag);
 
