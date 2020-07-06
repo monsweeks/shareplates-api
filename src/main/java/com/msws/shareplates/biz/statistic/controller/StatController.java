8b10505e04bd4fc358172aa82d2af8354e0d040d
hyunseok <mostgreat@gmail.com>
2020-07-06 19:23:17 +0900

    profile : default 시에 stat 로그는 stdout.

--- src/main/java/com/msws/shareplates/biz/statistic/controller/StatController.java
+++ src/main/java/com/msws/shareplates/biz/statistic/controller/StatController.java
@@ -21,7 +21,9 @@ public class StatController {
 
 	@DisableLogin
 	@RequestMapping( path="/shares/{shareId}", method = RequestMethod.GET, produces = "application/json")
-	public List<UserAccessCount> stats(@PathVariable Long shareId, @RequestParam(value = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)  LocalDateTime startDate, @RequestParam(value = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate ) {
+	public List<UserAccessCount> stats(@PathVariable Long shareId, 
+			                           @RequestParam(value = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)  LocalDateTime startDate, 
+			                           @RequestParam(value = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate ) {
 		// TODO timeunit은 startDate, endDate 차이에 따라서 서버에서 결정, 최대 조회되는 시간 포인트가 50이 넘지않도록? 8시간의 경우 10분 단위로 48개, 24시간의 경우, 30분 단위로 48개 정도로~
 		return null;
 	}
